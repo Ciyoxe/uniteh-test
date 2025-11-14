93000adb201a001664fd721d597668babe1ba06a
ciyoxe <ciyoxe@gmail.com>
2025-11-14 18:40:32 +0700

    Added yandex-maps library, added env config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
-import { createApp } from 'vue';
+import { createYmaps } from 'vue-yandex-maps';
 import { createPinia } from 'pinia';
+import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).use(createPinia().use(piniaPluginPersistedstate)).mount('#app');
+createApp(App)
+    .use(createPinia().use(piniaPluginPersistedstate))
+    .use(
+        createYmaps({
+            apikey: import.meta.env.VITE_YANDEX_MAPS_API_KEY,
+        }),
+    )
+    .mount('#app');
